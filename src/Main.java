36cd9b79d0ea97c99b3b2dd39bfa87e628111caa
Rogerio pc Interativa <rogerioo.lara@hotmail.com>
2024-01-31 16:06:44 -0300

    DB Integrity - rollback, commit, autocommit
    Signed-off-by: Rogerio pc Interativa <rogerioo.lara@hotmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import db.DB;
+import db.DbException;
+import db.DbIntegrityException;
 
 import java.sql.*;
 import java.text.ParseException;
@@ -11,7 +13,15 @@ public class Main {
         // Connection conn = DB.getConnection();
         // DB.closeConnection();
 
-        atualizarDados();
+        /*
+        Transações (ACID):
+        Atomicidade: ou acontece tudo ou não acontece nada
+        Consinstência
+        Isolada
+        Durável
+         */
+
+        transacoes();
 
     }
 
@@ -141,6 +151,78 @@ public class Main {
     public static void deletarDados(){
         Connection conn = null;
 
+        PreparedStatement st = null;
 
+        try {
+            conn = DB.getConnection();
+
+            st = conn.prepareStatement(
+                "DELETE FROM department "
+                + "WHERE "
+                + "Id = ?"
+            );
+
+            st.setInt(1, 2);
+
+            int rowsAffected = st.executeUpdate();
+
+            System.out.println("Done! Rows affected: " + rowsAffected);
+
+        } catch (SQLException e){
+            throw new DbIntegrityException(e.getMessage());
+        } finally {
+//            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+
+
+    }
+
+    public static void transacoes(){
+        Connection conn = null;
+        Statement st = null;
+
+        try{
+            conn = DB.getConnection();
+
+            // transação - não confirma a operação automaticamente
+            conn.setAutoCommit(false);
+
+            st = conn.createStatement();
+
+            int rows1 = st.executeUpdate(
+                "UPDATE seller SET BaseSalary = 2090.0 WHERE DepartmentId = 1");
+
+            /*
+            lançando um erro entre as transações para teste
+            int x = 1;
+            if (x < 2){
+                throw new SQLException("Fake error");
+            }
+            */
+
+
+            int rows2 = st.executeUpdate(
+                "UPDATE seller SET BaseSalary = 3090.0 WHERE DepartmentId = 2");
+
+            // confirma a transação
+            conn.commit();
+
+            System.out.println("rows1: " + rows1);
+            System.out.println("rows2: " + rows2);
+
+        } catch (SQLException e){
+            // faz um rollback caso ocorra erro
+            try {
+                conn.rollback();
+                throw new DbException("Transaction rolled back! Caused by: " + e.getMessage());
+            } catch (SQLException ex) {
+                throw new DbException("Error trying to rollback! Caused by: " + e.getMessage());
+            }
+        } finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
     }
 }
